09758ef74851ebe3277f50e5124047cd9cdd2f42
JacobHaake <jhaake18@gmail.com>
2019-06-05 01:26:14 -0400

    Fixed ContactList table

--- client/src/Components/ContactList.js
+++ client/src/Components/ContactList.js
@@ -25,8 +25,6 @@ class ContactList extends Component {
 
     }
 
-    
-
     componentDidMount() {
         this.getContacts();
     }
@@ -34,7 +32,7 @@ class ContactList extends Component {
     getContacts = async() => {
         const response = await axios.get('http://localhost:3001/api/contact/'+ this.state.userId + '/getContacts/')
         console.log(response);
-        this.setState({contacts : response.contacts});
+        this.setState({contacts : response.data.contacts});
     }
 
     contactList()
@@ -43,45 +41,52 @@ class ContactList extends Component {
             return <Contact contact={currentContact} key={i} />;
         })
     }
+
+    renderContact(contact, index)
+    {
+        return (
+            <tr key={index}>
+                <td color="#0FF000"><font color="#FFFFFF">{contact.contact_name}</font></td>
+                <td color="#0FF000"><font color="#FFFFFF">{contact.contact_phone}</font></td>
+                <td color="#0FF000"><font color="#FFFFFF">{contact.contact_email}</font></td>
+            </tr>
+        )
+    }
 	
     render()
     {
         console.log("Current user's ID " + this.state.userId);
 
+        const data = this.contactList();
+
         return (
             <div className="App">
-
-    
                 <div className="App__Aside">
-
                 </div>
-		
-
                 <div className="App__Form">
-         
                     <label><font size="6">Contact Manager</font></label>
-          	        <div className="PageSwitcher">
-            		    <NavLink to="/contactList" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Contacts</NavLink>		
-            		    <NavLink exact to="/createContact" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Create New Contact</NavLink>
-          		    </div>
+                    <div className="PageSwitcher">
+                        <NavLink to="/contactList" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Contacts</NavLink>		
+                        <NavLink exact to="/createContact" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Create New Contact</NavLink>
+                    </div>
                     <div>
-                    <h3>Contacts List</h3>
-                    <table className="table table-striped" style={{ marginTop: 20 }}>
-                        <thead>
-                            <tr>
-                                <th color="#FFFFFF"><font color="#FFFFFF">Name</font></th>
-                                <th color="#FFFFFF"><font color="#FFFFFF">Number</font></th>
-                                <th color="#FFFFFF"><font color="#FFFFFF">Email</font></th>
-                                <th color="#FFFFFF"><font color="#FFFFFF">Action</font></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* { this.contactList() } */}
-                        </tbody>
-                    </table>
-                </div>	
+                        <h3>Contacts List</h3>
+                        <table className="table table-striped" style={{ marginTop: 20 }}>
+                            <thead>
+                                <tr>
+                                    <th color="#FFFFFF"><font color="#FFFFFF">Name</font></th>
+                                    <th color="#FFFFFF"><font color="#FFFFFF">Number</font></th>
+                                    <th color="#FFFFFF"><font color="#FFFFFF">Email</font></th>
+                                    <th color="#FFFFFF"><font color="#FFFFFF">Action</font></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.contacts.map(this.renderContact)}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-        </div>
         );
     }
 }
